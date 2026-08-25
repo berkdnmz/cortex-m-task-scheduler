3ae01755f6044dabb334ba54135691a9d4291cd4
Berk Dönmez <220366455+berkdnmz@users.noreply.github.com>
2026-08-25 11:06:06 +0300

    feat: define task stack boundaries and declare user tasks

--- Src/main.c
+++ Src/main.c
@@ -17,13 +17,67 @@
  */
 
 #include <stdint.h>
+#include <stdio.h>
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+void task1_handler(void); //This is task1
+void task2_handler(void); //This is task2
+void task3_handler(void); //This is task3
+void task4_handler(void); //This is task4 of the application
+
+/*
+ * Some stack memory calculations
+ */
+#define SIZE_TASK_STACK					1024U
+#define SIZE_SCHED_STACK				1024U
+
+#define SRAM_START						0X20000000U
+#define SIZE_SRAM						( (128) * (1024))
+#define SRAM_END						( (SRAM_START) + (SIZE_SRAM) )
+
+#define T1_STACK_START					SRAM_END
+#define T2_STACK_START					( (SRAM_END) - (1 * SIZE_TASK_STACK) )
+#define T3_STACK_START					( (SRAM_END) - (2 * SIZE_TASK_STACK) )
+#define T4_STACK_START					( (SRAM_END) - (3 * SIZE_TASK_STACK) )
+#define SCHED_STACK_START				( (SRAM_END) - (4 * SIZE_TASK_STACK) )
+
 int main(void)
 {
     /* Loop forever */
 	for(;;);
+}
+
+void task1_handler(void)
+{
+	while(1)
+	{
+		printf("This is task1\n");
+	}
+}
+
+void task2_handler(void)
+{
+	while(1)
+	{
+		printf("This is task2\n");
+	}
+}
+
+void task3_handler(void)
+{
+	while(1)
+	{
+		printf("This is task3\n");
+	}
+}
+
+void task4_handler(void)
+{
+	while(1)
+	{
+		printf("This is task4\n");
+	}
 }
