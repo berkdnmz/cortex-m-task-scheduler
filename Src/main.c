fe0bd0b6c0e50fe6d116c2045e91ce7cd887e8ea
Berk Dönmez <220366455+berkdnmz@users.noreply.github.com>
2026-08-26 10:16:32 +0300

    feat: implement SysTick handler and context switching for multi-tasking

--- Src/main.c
+++ Src/main.c
@@ -162,6 +162,17 @@ uint32_t get_psp_value(void)
 	return psp_of_tasks[current_task];
 }
 
+void save_psp_value(uint32_t current_psp_value)
+{
+	psp_of_tasks[current_task] = current_psp_value;
+}
+
+void update_next_task(void)
+{
+	current_task++;
+	current_task %= MAX_TASKS;
+}
+
 __attribute__ ((naked)) void switch_sp_to_psp(void)
 {
 	//1. initialize the PSP with TASK1 stack start
@@ -178,9 +189,40 @@ __attribute__ ((naked)) void switch_sp_to_psp(void)
 	__asm volatile ("BX LR");
 }
 
-void SysTick_Handler(void)
+__attribute__ ((naked)) void SysTick_Handler(void)
 {
+	/*
+	 * Save the context of current task
+	 */
+	//1. Get current running task's PSP value
+	__asm volatile("MRS R0, PSP");
 
+	//2. Using that PSP value store SF2( R4 to R11)
+	__asm volatile("STMDB R0!, {R4-R11}");
+
+	__asm volatile("PUSH {LR}");
+
+	//3. Save the current value of PSP
+	__asm volatile("BL save_psp_value");
+
+	/*
+	 * Retrieve the context of next task
+	 */
+	//1. Decide next task to run
+	__asm volatile("BL update_next_task");
+
+	//2. Get its past PSP value
+	__asm volatile("BL get_psp_value");
+
+	//3. Using that PSP value retrieve SF2(R4 to R11)
+	__asm volatile("LDMIA R0!,{R4-R11}");
+
+	//4. Update PSP and exit
+	__asm volatile("MSR PSP, R0");
+
+	__asm volatile("POP {LR}");
+
+	__asm volatile("BX LR");
 }
 
 void HardFault_Handler(void)
